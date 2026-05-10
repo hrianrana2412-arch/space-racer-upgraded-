05ca52e77785ff4552c83904bb1866a7f89b7385
hrianrana2412-arch <hrianrana2412@gmail.com>
2026-05-10 10:34:25 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log('Space Racer running on ' + PORT));
